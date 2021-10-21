645201ce09b1c83dd9e7f63a560fcbdf20d724ed
aivnerrad <aivnerrad1993@gmail.com>
2021-10-20 22:54:55 -0400

    more css done

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,49 +1,61 @@
-
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faEdit, faUsers } from '@fortawesome/free-solid-svg-icons'
 import './NavBar.css'
+import SearchBar from './SearchBar';
 
 const NavBar = () => {
-  const user = useSelector(state => state.session.user)
-  return (
-    <nav>
-      <div>
-        <NavLink id="home-button" to='/' exact={true} activeClassName='active'>
-          Viora
-        </NavLink>
-      </div>
-      <div id="home-icon-div">
-        <NavLink to='/' exact={true}>
-          <FontAwesomeIcon id="home-icon" icon={faHome} />
-        </NavLink>
-      </div>
-      <div id="answer-question-icon-div">
-        <NavLink to='/answer'>
-          <FontAwesomeIcon id="answer-icon" icon={faEdit} />
-        </NavLink>
-      </div>
-      {!user && <div>
-        <NavLink to='/login' exact={true} activeClassName='active'>
-          Login
-        </NavLink>
-      </div>}
-      {!user && <div>
-        <NavLink to='/sign-up' exact={true} activeClassName='active'>
-          Sign Up
-        </NavLink>
-      </div>}
-      <div>
-        <NavLink to='/users' exact={true} activeClassName='active'>
-          Users
-        </NavLink>
-      </div>
-      {user && <div> <LogoutButton /> </div>}
-    </nav>
-  );
+    const user = useSelector(state => state.session.user)
+    return (
+        <nav>
+            <div>
+                <NavLink id="home-button" to='/' exact={true} activeClassName='active'>
+                    Viora
+                </NavLink>
+            </div>
+            <div id="icon-div" >
+                <NavLink to='/'exact={true} >
+                    <FontAwesomeIcon id="icon" icon={faHome}/>
+                </NavLink>
+            </div>
+            <div id="icon-div" >
+                <NavLink to='/answer' >
+                    <FontAwesomeIcon id="icon" icon={faEdit}/>
+                </NavLink>
+            </div>
+            <div id="icon-div" >
+                <FontAwesomeIcon id="icon" icon={faUsers}/>
+            </div>
+            <div id="search-bar-div" >
+                <SearchBar />
+            </div>
+            {!user &&
+                <div>
+                    <NavLink to='/login' exact={true} activeClassName='active' >
+                        Login
+                    </NavLink>
+                </div>
+            }
+            {!user &&
+                <div>
+                    <NavLink to='/sign-up' exact={true} activeClassName='active' >
+                        Sign Up
+                    </NavLink>
+                </div>
+            }
+            <div id='my-profile-circle-div'>
+                <p>{user.firstName[0]}</p>
+            </div>
+            {user &&
+                <div>
+                    <LogoutButton />
+                </div>
+            }
+            </nav>
+    );
 }
 
 export default NavBar;
