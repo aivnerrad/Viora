70e5edc533e40c9e33c31c348d6140f8e601e5fb
aivnerrad <aivnerrad1993@gmail.com>
2021-10-28 12:48:56 -0400

    some final changes

--- react-app/src/components/SideBar/index.js
+++ react-app/src/components/SideBar/index.js
@@ -27,13 +27,13 @@ const SideBar = () => {
         })}
       </div>
       <div id="about-me-links">
-        <a target="_blank" rel="noreferrer" href="https://github.com/aivnerrad">Check out my projects on Github!</a>
+        <a id="my-button" target="_blank" rel="noreferrer" href="https://github.com/aivnerrad">Check out my projects on Github!</a>
         <br/>
         <br/>
-        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/darren-via-ii-552667159/">Connect with me on LinkedIn!</a>
+        <a id="my-button" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/darren-via-ii-552667159/">Connect with me on LinkedIn!</a>
         <br/>
         <br/>
-        <a target="_blank" rel="noreferrer" href="https://twitter.com/DarrenVia">Follow Me on Twitter!</a>
+        <a id="my-button" target="_blank" rel="noreferrer" href="https://twitter.com/DarrenVia">Follow Me on Twitter!</a>
       </div>
     </div>
     </>
